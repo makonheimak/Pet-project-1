59d05ea01ecd2df9ceba134905ced55c35c4d576
makonheimak <114416982+makonheimak@users.noreply.github.com>
2025-07-02 14:11:30 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-var task string
-
 func main() {
 	e := echo.New()
 
@@ -16,13 +14,17 @@ func main() {
 	e.Use(middleware.Logger())
 
 	e.POST("/task", postTask)
-	e.GET("/task", getTask)
+	e.GET("/task", getTasks)
+	e.PATCH("/task/:id", patchTask)
+	e.DELETE("/task/:id", deleteTask)
 
 	e.Start("localhost:8080")
-
 }
 
+var tasks = []Task{}
+
 type Task struct {
+	ID   string `json:"id"`
 	Task string `json:"task"`
 }
 
@@ -32,11 +34,45 @@ func postTask(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid json"})
 	}
 
-	task = req.Task
-	return c.JSON(http.StatusOK, map[string]string{"status": "task saved"})
+	tasks = append(tasks, req)
 
+	return c.JSON(http.StatusOK, map[string]string{"status": "task saved"})
 }
 
-func getTask(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]string{"message": "hell привет, " + task})
+func getTasks(c echo.Context) error {
+	return c.JSON(http.StatusOK, tasks)
+}
+
+func patchTask(c echo.Context) error {
+	id := c.Param("id")
+
+	var req Task
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid request"})
+	}
+
+	for i, t := range tasks {
+		if t.ID == id {
+			tasks[i].Task = req.Task
+			return c.JSON(http.StatusOK, tasks[i])
+		}
+	}
+	return c.JSON(http.StatusNotFound, map[string]string{"error": "task not found"})
+}
+
+func deleteTask(c echo.Context) error {
+	id := c.Param("id")
+
+	var req Task
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "task not found"})
+	}
+
+	for i, t := range tasks {
+		if t.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			return c.NoContent(http.StatusNoContent)
+		}
+	}
+	return c.JSON(http.StatusNotFound, map[string]string{"error": "task not found"})
 }
